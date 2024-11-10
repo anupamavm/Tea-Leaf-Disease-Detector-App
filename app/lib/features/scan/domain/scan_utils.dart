790e60d864bf5baf046d44e5a2dae9d495732873
Anupama Morapitiya <anupamamorapitiya97@gmail.com>
2024-11-10 15:19:46 +0530

    predictions work for online offline mode

--- app/lib/features/scan/domain/scan_utils.dart
+++ app/lib/features/scan/domain/scan_utils.dart
@@ -4,6 +4,7 @@ import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 import 'connectivity_utils.dart'; // Import connectivity utilities
+import 'dart:convert';
 // Import the ResultsPage for navigation
 
 // Initialize the TFLite model
@@ -48,36 +49,18 @@ Future<List<dynamic>?> runTFLiteModel(XFile capturedImage) async {
   }
 }
 
-// Scan image function
 Future<void> scanImage(
     BuildContext context, XFile? capturedImage, String? location) async {
   try {
-    debugPrint("Starting scanImage function...");
     if (capturedImage != null) {
-      debugPrint("Captured image path: ${capturedImage.path}");
-
-      // Initialize model if not loaded
       await _tfLteInit();
-
-      // Check internet connection
-      debugPrint("Checking internet connection...");
       bool isConnected = await hasInternetConnection();
 
-      // If there's no internet connection, use the local TFLite model
       if (!isConnected) {
-        debugPrint("No internet connection. Running local TFLite model...");
-        var predictions = await runTFLiteModel(
-            capturedImage); // Run the local TFLite model and get predictions
-        debugPrint("Predictions from TFLite model: $predictions");
-
+        var predictions = await runTFLiteModel(capturedImage);
         if (predictions != null && predictions.isNotEmpty) {
-          // Assuming you want the first prediction result
           String result = predictions[0]['label'];
           double confidence = predictions[0]['confidence'];
-
-          debugPrint("Prediction result: $result, Confidence: $confidence");
-
-          // Navigate to ResultsPage and pass the prediction results
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -91,9 +74,20 @@ Future<void> scanImage(
           debugPrint("No predictions found from TFLite model.");
         }
       } else {
-        // If there's an internet connection, send the image to the server
-        debugPrint("Internet connection detected. Sending image to server...");
-        await sendDataToServer(capturedImage, location);
+        // Send image to server and get response
+        var serverResponse = await sendDataToServer(capturedImage, location);
+        if (serverResponse != null) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ResultsPage(
+                result: serverResponse['disease'],
+                confidence: serverResponse['score'] *
+                    100, // convert to percentage if needed
+              ),
+            ),
+          );
+        }
       }
     } else {
       debugPrint("Captured image is null.");
@@ -103,39 +97,33 @@ Future<void> scanImage(
   }
 }
 
-Future<void> sendDataToServer(XFile capturedImage, String? location) async {
+Future<Map<String, dynamic>?> sendDataToServer(
+    XFile capturedImage, String? location) async {
   try {
-    // Ensure the captured image path is valid
     if (capturedImage.path.isNotEmpty &&
         location != null &&
         location.isNotEmpty) {
-      // Prepare a multipart request
       var request = http.MultipartRequest(
         'POST',
-        Uri.parse(
-            'http://35.244.22.197/upload'), // Update with your backend URL
+        Uri.parse('http://35.244.22.197/upload'),
       );
 
-      // Convert the image file to a multipart file
-      var file = await http.MultipartFile.fromPath(
-        'file', // The name of the file parameter in the backend
-        capturedImage.path,
-      );
-
-      // Add the file and location fields to the request
+      var file = await http.MultipartFile.fromPath('file', capturedImage.path);
       request.files.add(file);
       request.fields['location'] = location;
 
-      // Send the request and wait for the response
       var response = await request.send();
-
-      // Get the response body if needed
       var responseBody = await response.stream.bytesToString();
 
-      // Handle the response
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         debugPrint('Data sent to server successfully');
-        debugPrint('Server response: $responseBody');
+        var jsonResponse = json.decode(responseBody);
+
+        // Extract disease and score from the response
+        return {
+          'disease': jsonResponse['huggingface_result']['disease'],
+          'score': jsonResponse['huggingface_result']['score'],
+        };
       } else {
         debugPrint('Failed to send data. Status code: ${response.statusCode}');
         debugPrint('Error response: $responseBody');
@@ -146,4 +134,5 @@ Future<void> sendDataToServer(XFile capturedImage, String? location) async {
   } catch (e) {
     debugPrint('Error sending data to server: $e');
   }
+  return null;
 }
--- app/lib/features/scan/presentation/results.dart
+++ app/lib/features/scan/presentation/results.dart
@@ -12,28 +12,30 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Prediction Results"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Prediction: $result',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Confidence: ${confidence.toStringAsFixed(2)}%',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Navigate back
-              },
-              child: Text("Back"),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Prediction: $result',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Confidence: ${confidence.toStringAsFixed(2)}%',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Navigate back
+                },
+                child: Text("Back"),
+              ),
+            ],
+          ),
         ),
       ),
     );
